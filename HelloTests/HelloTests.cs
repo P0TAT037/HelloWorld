5f6daa40de984029ff2da800a7f9830fa461c3aa
P0TAT037 <77874294+P0TAT037@users.noreply.github.com>
2022-12-09 15:56:52 +0200

    Add project files.

---
+++ HelloTests/HelloTests.cs
@@ -0,0 +1,18 @@
+using Xunit;
+using HelloWorld;
+
+namespace HelloTests
+{
+    public class HelloTests
+    {
+        [Fact]
+        public void test()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("end");
+           
+        }
+    }
+}
